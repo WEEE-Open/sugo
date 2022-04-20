e948e765ccb24350b87db568f47be6655a20da37
il-palmi <il-palmi@users.noreply.github.com>
2022-04-20 19:51:57 +0200

    Added pdf viewer functionality

--- sugo.py
+++ sugo.py
@@ -1,6 +1,10 @@
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import os
+import mimetypes
+from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets, uic
 from PyQt5.QtCore import Qt
+import pdf2image
+
 
 MOUSE_THRESHOLD = 3
 
@@ -9,12 +13,37 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi("assets/main.ui", self)
+        self.setAcceptDrops(True)
+        self.pdfViewer = None
 
-        self.imageLabel = self.findChild(QtWidgets.QLabel, "imageLabel")
-        self.show()
+        self.promptLabel = self.findChild(QtWidgets.QLabel, "promptLabel")
+        self.imageLabel = DragAndDropLabel("assets/upload.jpg")
+        self.imageLabel.triggered.connect(self.load_document)
+        self.centralWidget().layout().addWidget(self.imageLabel)
         self.signWidget = SignWidget()
         self.signWidget.update.connect(self.show_sign)
 
+        self.clearShortcut = QtWidgets.QShortcut(QtGui.QKeySequence("Ctrl+L"), self)
+        self.clearShortcut.activated.connect(self.ask_sign)
+
+        self.show()
+
+    def load_document(self, path: str):
+        if path == "":
+            path = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', "~", "PDF (*.pdf)")[0]
+            if path == "":
+                return
+        if os.path.isfile(path):
+            mtypes = mimetypes.guess_type(path)
+            if mtypes and 'application/pdf' in mtypes:
+                self.imageLabel.hide()
+                self.promptLabel.hide()
+                self.pdfViewer = PdfViewerEngine(path)
+                # PdfViewer(path)
+                self.centralWidget().layout().addWidget(self.pdfViewer)
+
+    def ask_sign(self):
+        self.signWidget.show()
 
     def show_sign(self, pixmap: QtGui.QPixmap):
         self.imageLabel.setPixmap(pixmap)
@@ -37,8 +66,6 @@ class SignWidget(QtWidgets.QWidget):
         self.confirmButton.clicked.connect(self.confirm_sign)
         self.cancelButton = self.findChild(QtWidgets.QPushButton, "cancelButton")
         self.cancelButton.clicked.connect(self.close)
-
-        self.show()
 
     def confirm_sign(self):
         signPixmap = QtGui.QPixmap(self.graphicsView.viewport().size())
@@ -80,6 +107,40 @@ class GraphicsScene(QtWidgets.QGraphicsScene):
 
     def mouseReleaseEvent(self, event: 'QGraphicsSceneMouseEvent') -> None:
         self.lastMousePosition = None
+
+
+class DragAndDropLabel(QtWidgets.QLabel):
+    triggered = QtCore.pyqtSignal(str)
+
+    def __init__(self, image_path: str):
+        super(DragAndDropLabel, self).__init__()
+        self.setAcceptDrops(True)
+        self.setAlignment(Qt.AlignHCenter)
+        pixmap = QtGui.QPixmap(image_path)
+        self.setPixmap(pixmap.scaledToWidth(500, Qt.SmoothTransformation))
+
+    def mousePressEvent(self, ev: QtGui.QMouseEvent) -> None:
+        self.triggered.emit("")
+
+    def dragEnterEvent(self, a0: QtGui.QDragEnterEvent) -> None:
+        a0.acceptProposedAction()
+
+    def dropEvent(self, a0: QtGui.QDropEvent) -> None:
+        a0.acceptProposedAction()
+        self.triggered.emit(a0.mimeData().text().lstrip("file:///"))
+
+
+class PdfViewer(QtWidgets.QScrollArea):
+    def __init__(self, path: str):
+        super(PdfViewer, self).__init__()
+        self.pdfPages = pdf2image.convert_from_path(path)
+
+
+class PdfViewerEngine(QtWebEngineWidgets.QWebEngineView):
+    def __init__(self, path: str):
+        super(PdfViewerEngine, self).__init__()
+        self.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.PluginsEnabled, True)
+        self.load(QtCore.QUrl.fromLocalFile(path))
 
 
 def main():
